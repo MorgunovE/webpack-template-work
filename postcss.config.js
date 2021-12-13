bd6091a1572dcc18a9934309959bb97bd15bbee5
morgunove <morgunove87@gmail.com>
2021-12-13 19:49:47 +0500

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true
+          }
+        }
+      ]
+    })
+  ]
+}
